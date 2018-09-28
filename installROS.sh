09074aeb90e6352766f391b9513864d5e96d9bff
Akashbaskaran <akashbaskaran@gmail.com>
2018-09-28 17:03:48 -0500

    Add files via upload

---
+++ installROS.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Install Robot Operating System (ROS) on NVIDIA Drive PX 2
+# Maintainer of ARM builds for ROS is http://answers.ros.org/users/1034/ahendrix/
+# Information from:
+# http://wiki.ros.org/kinetic/Installation/UbuntuARM
+
+# The script is modified according to discussions on the Nvidia forum: https://devtalk.nvidia.com/default/topic/1011002/general/cross-compiling-driveworks-on-the-px2-with-ros/
+# The instructions from the ROS's official site can also be followed 
+
+# Setup sources.lst
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+# Setup keys
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
+# Update the libssl1.0.0 package to the latest version [1.0.2g-1ubuntu4.6]and install libssl-dev
+sudo apt-get install libssl1.0.0/xenial libssl-doc/xenial libssl-dev/xenial
+# Installation
+sudo apt-get update
+# sudo apt-get install ros-kinetic-ros-base -y
+sudo apt-get install ros-kinetic-desktop
+# Initialize rosdep
+sudo apt-get install python-rosdep -y
+# Certificates are messed up
+sudo c_rehash /etc/ssl/certs
+# Initialize rosdep
+sudo rosdep init
+# To find available packages, use:
+rosdep update
+# Environment Setup
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+# echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib:/usr/lib/aarch64-linux-gnu" >> ~/.bashrc
+source ~/.bashrc
+# Install rosinstall
+sudo apt-get install python-rosinstall -y
